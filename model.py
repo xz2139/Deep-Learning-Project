6ec4b99c98f798cd1c5c9d11678957690dc8f910
Will Zhang <willworld9422@gmail.com>
2018-04-18 01:05:20 -0400

    minor change

--- model.py
+++ model.py
@@ -1,14 +1,13 @@
-from __future__ import print_function, division
-
 import torch
 import torch.nn as nn
 import numpy as np
 import torchvision
 from torchvision import models
+from torch.autograd import Variable
 
 
 class Image_Encoder_CNN(nn.Module):
-    
+
     def __init__(self, input_size):
         super(Image_Encoder_CNN, self).__init__()
         vgg11 = models.vgg11(pretrained=True)
@@ -17,11 +16,11 @@ class Image_Encoder_CNN(nn.Module):
         self.linear = nn.Linear(vgg11.classifier[0].out_features, input_size)
         self.bn = nn.BatchNorm1d(input_size, momentum=0.01)
         self.init_weights()
-    
+
     def init_weights(self):
         self.linear.weight.data.normal_(0.0, 0.02)
         self.linear.bias.data.fill_(0)
-    
+
     def forward_vgg(self, image):
         imfeatures = self.vgg11(image)
         imfeatures = Variable(imfeatures.data)
@@ -31,25 +30,25 @@ class Image_Encoder_CNN(nn.Module):
 
 
 class Image_Encoder_LSTM(nn.Module):
-    
+
     def __init__(self, embedding_size, hidden_size, num_layers, use_cuda):
+        super(Image_Encoder_LSTM, self).__init__()
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.use_cuda = use_cuda
         self.lstm = nn.LSTM(embedding_size, hidden_size, num_layers, batch_first=True)
         self.init_weights()
-    
+
     def init_hidden(self, imfeatures):
         hidden = (Variable(torch.zeros(self.num_layers.n_direction, imfeatures.size(0), self.hidden_size)), Variable(torch.zeros(self.n_layers.n_direction, imfeatures.size(0), self.hidden_size)))
         return (hidden[0].cuda(), hidden[1].cuda()) if use_cuda else hidden
-    
-    def init_weight(self):
+
+    def init_weights(self):
         self.lstm.weight_hh_l0 = nn.init.xavier_uniform(self.lstm.weight_hh_l0)
         self.lstm.weight_ih_l0 = nn.init.xavier_uniform(self.lstm.weight_ih_l0)
-    
+
     def forwar_lstm(self, imfeatures):
         hidden, _ = self.lstm(imfeatures)
         output = hidden[-1]
         return output
-
